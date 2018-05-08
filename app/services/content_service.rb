4c0ffe7c395547c75017c9dc6724c2973173774e
Mikhail Dyomin <mikhaildyomin@gmail.com>
2018-05-08 09:42:11 +0300

    Implement service to pull data from contentful

---
+++ app/services/content_service.rb
@@ -0,0 +1,35 @@
+class ContentService
+  class << self
+    def categories
+      client.entries(content_type: 'category', 'fields.availableOn[in]' => ['infotoss.com'])
+    end
+
+    def category(title)
+      client.entries(content_type: 'category',
+                     'fields.availableOn[in]' => ['infotoss.com'],
+                     'fields.title' => title).first
+    end
+
+    def articles(category_title, page)
+      client.entries(content_type: 'article', limit: 8, skip: (page.to_i - 1) * 8,
+                     'fields.availableOn[in]' => ['infotoss.com'],
+                     'fields.categories.sys.id' => category(category_title).id)
+    end
+
+    def article(slug)
+      client.entries(content_type: 'article',
+                     'fields.availableOn[in]' => ['infotoss.com'],
+                     'fields.slug' => slug).first
+    end
+
+    private
+
+    def client
+      @_client = Contentful::Client.new(
+        access_token: Rails.application.credentials.contentful_access_token,
+        space: Rails.application.credentials.contentful_space,
+        max_include_resolution_depth: 1
+      )
+    end
+  end
+end
